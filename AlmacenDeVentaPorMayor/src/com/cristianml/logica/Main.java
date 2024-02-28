24e676197190d6cf589522681366d3025c353921
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-02-28 15:18:57 -0400

    Ejercicio Almacen, practicando Herencia de las clases.

---
+++ AlmacenDeVentaPorMayor/src/com/cristianml/logica/Main.java
@@ -0,0 +1,16 @@
+package com.cristianml.logica;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Sólo vemos la jerarquía de las clases
+        public Class Producto {}
+        public Class Perecible extends Producto {}
+        public Class NoPerecible extends Producto {}
+        public Class Limpieza extends Producto {}
+
+        public Class Persona {}
+        public Class Vendedor extends Persona {}
+        public Class Cliente extends Persona {}
+    }
+}
